54bc5ef175f0a302a6dc7209fef133574bd864e8
plule <630159+plule@users.noreply.github.com>
2023-04-24 20:15:50 +0200

    Reduce binary size

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 mod app;
+#[cfg(feature = "cli")]
 mod cli;
 pub mod export;
 pub mod map;
@@ -9,37 +10,38 @@ pub mod rfr;
 pub mod tile;
 pub mod update;
 use app::App;
-use clap::Parser;
-use cli::{run_cli_command, Cli};
 use eframe::egui;
 
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 const ICON: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/icon"));
 
 fn main() -> anyhow::Result<()> {
-    let cli = Cli::parse();
+    #[cfg(feature = "cli")]
+    {
+        use clap::Parser;
+        let cli = cli::Cli::parse();
 
-    if let Some(command) = cli.command {
-        run_cli_command(command)
-    } else {
-        // Log to stdout (if you run with `RUST_LOG=debug`).
-        tracing_subscriber::fmt::init();
-        let options = eframe::NativeOptions {
-            initial_window_size: Some(egui::vec2(320.0, 320.0)),
-            icon_data: Some(eframe::IconData {
-                rgba: ICON.to_vec(),
-                width: 256,
-                height: 256,
-            }),
-            ..Default::default()
-        };
-        match eframe::run_native(
-            format!("Vox Uristi v{VERSION}").as_str(),
-            options,
-            Box::new(|cc| Box::<App>::new(app::App::new(cc))),
-        ) {
-            Ok(_) => Ok(()),
-            Err(e) => Err(anyhow::format_err!("{}", e.to_string())),
+        if let Some(command) = cli.command {
+            return cli::run_cli_command(command);
         }
+    }
+    // Log to stdout (if you run with `RUST_LOG=debug`).
+    tracing_subscriber::fmt::init();
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(320.0, 320.0)),
+        icon_data: Some(eframe::IconData {
+            rgba: ICON.to_vec(),
+            width: 256,
+            height: 256,
+        }),
+        ..Default::default()
+    };
+    match eframe::run_native(
+        format!("Vox Uristi v{VERSION}").as_str(),
+        options,
+        Box::new(|cc| Box::<App>::new(app::App::new(cc))),
+    ) {
+        Ok(_) => Ok(()),
+        Err(e) => Err(anyhow::format_err!("{}", e.to_string())),
     }
 }
