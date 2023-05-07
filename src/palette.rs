350ae2150042d7de971cab770e8288228738bf65
plule <630159+plule@users.noreply.github.com>
2023-05-07 12:01:53 +0200

    Only export actually used palette colors

--- src/palette.rs
+++ src/palette.rs
@@ -2,16 +2,52 @@ use dfhack_remote::{MatPair, MaterialDefinition};
 use num_enum::IntoPrimitive;
 use rand::seq::SliceRandom;
 use std::collections::HashMap;
-use strum::{EnumCount, EnumIter, IntoEnumIterator};
+use strum::{EnumCount, EnumIter};
 use vox_writer::VoxWriter;
 
 /// A material to be exported as an entry in the palette
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub enum Material {
     /// Default material with hard-coded color
     Default(DefaultMaterials),
     /// Generic material built procedurally from Dwarf Fortress
     Generic(MatPair),
+}
+
+/// The default hard-coded materials
+#[derive(Debug, Clone, Copy, IntoPrimitive, EnumIter, EnumCount, Hash, PartialEq, Eq)]
+#[repr(u8)]
+pub enum DefaultMaterials {
+    /// Common material for all hidden tiles
+    Hidden,
+    Water,
+    Mist,
+    Magma,
+    Fire,
+    Smoke,
+    Miasma,
+    DarkGrass,
+    LightGrass,
+}
+
+impl Material {
+    pub fn get_color(&self, materials: &[MaterialDefinition]) -> (u8, u8, u8, u8) {
+        match self {
+            Material::Default(default) => default.get_color(),
+            Material::Generic(matpair) => materials
+                .iter()
+                .find(|m| matpair == m.mat_pair.get_or_default())
+                .map_or((0, 0, 0, 0), |material| {
+                    let color = &material.state_color;
+                    (
+                        color.red().try_into().unwrap_or_default(),
+                        color.green().try_into().unwrap_or_default(),
+                        color.blue().try_into().unwrap_or_default(),
+                        255,
+                    )
+                }),
+        }
+    }
 }
 
 // temp for trees
@@ -38,22 +74,6 @@ impl From<Material> for RandomMaterial {
     }
 }
 
-/// The default hard-coded materials
-#[derive(Debug, Clone, Copy, IntoPrimitive, EnumIter, EnumCount)]
-#[repr(u8)]
-pub enum DefaultMaterials {
-    /// Common material for all hidden tiles
-    Hidden,
-    Water,
-    Mist,
-    Magma,
-    Fire,
-    Smoke,
-    Miasma,
-    DarkGrass,
-    LightGrass,
-}
-
 impl DefaultMaterials {
     pub fn get_color(&self) -> (u8, u8, u8, u8) {
         match self {
@@ -72,46 +92,23 @@ impl DefaultMaterials {
 
 #[derive(Default)]
 pub struct Palette {
-    pub colors: HashMap<MatPair, u8>,
+    pub materials: HashMap<Material, u8>,
 }
 
 impl Palette {
     pub fn get_palette_color(&mut self, material: &Material) -> u8 {
-        1 + match material {
-            Material::Default(default_mat) => (*default_mat).into(),
-            Material::Generic(mat_pair) => {
-                let palette_size = self.colors.len();
-                *self.colors.entry(mat_pair.to_owned()).or_insert_with(|| {
-                    (DefaultMaterials::COUNT + palette_size)
-                        .try_into()
-                        .unwrap_or_default() // would be nice to warn in case of palette overflow
-                })
-            }
-        }
+        let palette_size = self.materials.len();
+        1 + *self
+            .materials
+            .entry(material.clone())
+            .or_insert_with(|| palette_size.try_into().unwrap_or_default()) // would be nice to warn in case of palette overflow
     }
 
     pub fn write_palette(&self, vox: &mut VoxWriter, materials: &[MaterialDefinition]) {
         vox.clear_colors();
-        for default_mat in DefaultMaterials::iter() {
-            let (r, g, b, a) = default_mat.get_color();
-            vox.add_color(r, g, b, a, default_mat.into());
-        }
-
-        // Custom colored
-        for (matpair, index) in self.colors.iter() {
-            let material = materials
-                .iter()
-                .find(|m| matpair == m.mat_pair.get_or_default());
-            if let Some(material) = material {
-                let color = &material.state_color;
-                vox.add_color(
-                    color.red().try_into().unwrap_or_default(),
-                    color.green().try_into().unwrap_or_default(),
-                    color.blue().try_into().unwrap_or_default(),
-                    255,
-                    *index,
-                );
-            }
+        for (material, index) in &self.materials {
+            let (r, g, b, a) = material.get_color(materials);
+            vox.add_color(r, g, b, a, *index);
         }
     }
 }
